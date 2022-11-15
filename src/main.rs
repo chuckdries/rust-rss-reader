63643effaf9e90e5743e23d400668019e14bc7d2
Chuck Dries <chuck@chuckdries.com>
2022-11-14 18:05:33 -0800

    format document

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 // use std::collections::HashMap;
-use feed_rs::{parser, model::{Entry, Feed}};
+use feed_rs::{
+    model::{Entry, Feed},
+    parser,
+};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let feed = get_feed("https://feeds.arstechnica.com/arstechnica/index").await.expect("Encountered an error getting feed");
-    
+    let feed = get_feed("https://feeds.arstechnica.com/arstechnica/index")
+        .await
+        .expect("Encountered an error getting feed");
+
     for entry in feed.entries {
         print_entry(&entry);
     }
@@ -13,25 +18,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 async fn get_feed(feed_url: &str) -> Result<Feed, reqwest::Error> {
-    let resp = reqwest::get(feed_url)
-        .await?
-        .text()
-        .await?;
+    let resp = reqwest::get(feed_url).await?.text().await?;
     let feed = parser::parse(resp.as_bytes()).expect("Failed to parse feed");
     Ok(feed)
 }
 
-fn print_entry (entry: &Entry) {
+fn print_entry(entry: &Entry) {
     match entry.title.as_ref() {
         Some(title) => {
             print!("• {}", title.content);
         }
-        None => ()
+        None => (),
     }
     match entry.links.get(0) {
         Some(link) => {
             println!(" - {}", link.href)
         }
-        None => println!("")
+        None => println!(""),
     }
 }
